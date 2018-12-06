471d2d9333d323698b6442ab555915e327b1d1af
freemanb2 <freemanb2@hotmail.com>
2018-12-06 08:54:19 -0500

    Added game sounds and esc key to exit game

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const electron = require('electron');
 const { app, BrowserWindow } = electron;
+const remote = require("electron").remote;
 
 app.on('ready', () => {
   let window = new BrowserWindow({ width: 1000, height: 800, frame: false});
   window.loadURL(`file://${__dirname}/index.html`);
   window.show();
+
+  const ret = electron.globalShortcut.register('Escape', function(){
+    window.close();
+  });
 })
 
 /*
